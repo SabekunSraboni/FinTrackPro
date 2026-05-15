96427be265672ae17a69067410d2e3c6c238496b
Sabekun Nahar Sraboni <srabonysayeds@gmail.com>
2026-05-15 22:19:42 +0600

    Update urls.py

--- TrustedProject/urls.py
+++ TrustedProject/urls.py
@@ -11,11 +11,10 @@ urlpatterns = [
     path('update/<int:pk>/', views.update_circle, name='update_circle'),
     path('delete/<int:pk>/', views.delete_circle, name='delete_circle'),
 
-    # মেম্বার ম্যানেজমেন্ট
+    
     path('add-member/<int:circle_id>/', views.add_member, name='add_member'),
     path('delete-member/<int:member_id>/', views.delete_member, name='delete_member'),
-
-    # এই লাইনটি মিসিং ছিল, তাই এরর দিচ্ছিল
+ 
     path('send-request/<int:circle_id>/', views.send_request, name='send_request'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
